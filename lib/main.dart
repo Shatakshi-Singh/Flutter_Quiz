687d07895b8706017a4c6a2e060ba3420d7d4c6e
Shatakshi Singh <s.shatakshi2103@gmail.com>
2021-07-05 00:26:00 +0530

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,42 @@ void main() {
 }
 
 class QuizApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Choosen');
+  }
+
   const QuizApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 3'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
